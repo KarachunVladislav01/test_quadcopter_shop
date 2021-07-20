85cf2b0cae77ef08d766546be5b0cf7bbf8f57bd
karachunvladislav <karachunvladislav@gmail.com>
2021-07-20 09:10:27 +0300

    feat: added font list

--- App.tsx
+++ App.tsx
@@ -7,6 +7,9 @@ import AppLoading from "expo-app-loading";
 export default function App() {
     let [fontsLoaded] = useFonts({
         "Lato-Regular": require("./src/assets/fonts/Lato/Lato-Regular.ttf"),
+        "Lato-Bold": require("./src/assets/fonts/Lato/Lato-Bold.ttf"),
+        "Lato-Semibold": require("./src/assets/fonts/Lato/Lato-Semibold.ttf"),
+        "Lato-Heavy": require("./src/assets/fonts/Lato/Lato-Heavy.ttf"),
     });
 
     return <>{fontsLoaded ? <Navigation /> : <AppLoading />}</>;
--- src/styles/fontClasses.ts
+++ src/styles/fontClasses.ts
@@ -1,11 +1,39 @@
 import { StyleSheet } from "react-native";
 
-import { _16, _22 } from "../components/constants/sizes";
+import {
+    _14,
+    _16,
+    _17,
+    _20,
+    _22,
+    _24,
+    _32,
+} from "../components/constants/sizes";
 
 export const fontClasses = StyleSheet.create({
     baseLine: {
         fontSize: _16,
         lineHeight: _22,
         fontFamily: "Lato-Regular",
+    },
+    boldSmall: {
+        fontSize: _16,
+        lineHeight: _22,
+        fontFamily: "Lato-Bold",
+    },
+    boldBig: {
+        fontSize: _20,
+        lineHeight: _24,
+        fontFamily: "Lato-Bold",
+    },
+    semiBold: {
+        fontSize: _14,
+        lineHeight: _17,
+        fontFamily: "Lato-Semibold",
+    },
+    extraBold: {
+        fontSize: _24,
+        lineHeight: _32,
+        fontFamily: "Lato-Heavy",
     },
 });
